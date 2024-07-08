f9567fd33e372ea870b34bd7bbbf23a935303bf8
georgeSTFC <77783716+georgeSTFC@users.noreply.github.com>
2024-07-08 14:51:32 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   env: {
     name: 'Hartree Kasm Registry',
     description: 'Hartree Kasm workspaces.',
-    icon: 'https://georgeSTFC.github.io//kasm-registry/1.0/image.png',
-    listUrl: 'https://georgeSTFC.github.io//kasm-registry/',
+    icon: 'https://georgeSTFC.github.io/kasm-registry/1.0/image.png',
+    listUrl: 'https://georgeSTFC.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
